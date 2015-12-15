06e6e1d28ea3ed63d17b4151d8616d38411f28f4
weibo.lei <weibo.lei@ikaowo.com>
2015-12-15 11:10:20 +0800

    the full screen theme

--- app/src/main/java/com/ikaowo/join/modules/user/SigninActivity.java
+++ app/src/main/java/com/ikaowo/join/modules/user/SigninActivity.java
@@ -1,6 +1,8 @@
 package com.ikaowo.join.modules.user;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 
 import com.ikaowo.join.BaseActivity;
 import com.ikaowo.join.R;
@@ -15,6 +17,11 @@ public class SigninActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
