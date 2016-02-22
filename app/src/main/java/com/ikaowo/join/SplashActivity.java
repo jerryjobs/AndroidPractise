5e67a55a7cf2a875859745acef7611f2295c0b5b
weibo.lei <weibo.lei@ikaowo.com>
2016-02-22 09:42:25 +0800

    tmp commit

---
+++ app/src/main/java/com/ikaowo/join/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.ikaowo.join;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.common.framework.core.JApplication;
+
+/**
+ * Created by weibo on 16-2-16.
+ */
+public class SplashActivity extends BaseActivity {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    new Handler().postDelayed(new Runnable() {
+      @Override public void run() {
+        Intent intent = new Intent(SplashActivity.this, MainTabActivity.class);
+        JApplication.getJContext().startActivity(SplashActivity.this, intent);
+        finish();
+      }
+    }, 3000);
+  }
+
+  @Override protected String getTag() {
+    return "SplashActivity";
+  }
+}
